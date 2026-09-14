11cce6b5e2412800b2ab40aa5ac3862eaaf13569
ViniiR <cvrm2004@gmail.com>
2026-09-14 19:53:01 -0300

    feat: create package for interface

--- flake.nix
+++ flake.nix
@@ -7,76 +7,97 @@
         system = "x86_64-linux";
         pkgs = import nixpkgs {inherit system;};
         lib = pkgs.lib;
-        interface_name = "com.vinii.vgsc";
-        binary_name = "vgscd";
         #
-        runtimePackages = with pkgs; [
+        interface_name = "com.vinii.vgsc";
+        binary_daemon_name = "vgscd";
+        binary_ui_name = "vgsc";
+        version = "0.1.0";
+        #
+        daemonRuntimePackages = with pkgs; [
             gawk
             jq
             pipewire
             wireplumber
         ];
     in {
-        packages.${system}.default = pkgs.stdenv.mkDerivation rec {
-            pname = binary_name;
-            version = "0.1";
+        packages.${system} = {
+            interface = pkgs.rustPlatform.buildRustPackage {
+                pname = binary_ui_name;
+                inherit version;
 
-            src = pkgs.nix-gitignore.gitignoreSource [] ./.;
+                src = pkgs.nix-gitignore.gitignoreSource [] ./.;
 
-            cmakeFlags = [
-                "--preset"
-                "release"
-            ];
+                cargoLock.lockFile = ./Cargo.lock;
 
-            nativeBuildInputs = with pkgs; [
-                pkg-config
-                cmake
-                gcc
+                nativeBuildInputs = with pkgs; [
+                    pkg-config
+                    glib
+                    wrapGAppsHook4
+                ];
+                buildInputs = with pkgs; [
+                    gdk-pixbuf
+                    gtk4
+                    glib
+                ];
+            };
+            daemon = pkgs.stdenv.mkDerivation rec {
+                pname = binary_daemon_name;
+                inherit version;
 
-                # TODO: integrate rust stuff here
-                # rustc
-                # cargo
-            ];
-            buildInputs = with pkgs;
-                [
-                    # Libraries
-                    systemd
-                    dbus
-                ]
-                ++ runtimePackages;
+                src = pkgs.nix-gitignore.gitignoreSource [] ./.;
 
-            installPhase = ''
-                runHook preInstall
+                cmakeFlags = [
+                    "--preset"
+                    "release"
+                ];
 
-                mkdir -p $out/bin
-                mv vgsc $out/bin/${pname}
+                nativeBuildInputs = with pkgs; [
+                    pkg-config
+                    cmake
+                    gcc
+                ];
+                buildInputs = with pkgs;
+                    [
+                        # Libraries
+                        systemd
+                        dbus
+                    ]
+                    ++ daemonRuntimePackages;
 
-                runHook postInstall
-            '';
+                installPhase = ''
+                    runHook preInstall
 
-            postInstall = ''
-                # Launch D-Bus service
-                mkdir -p $out/share/dbus-1/system-services
-                cat <<END > $out/share/dbus-1/system-services/${interface_name}.service
-                [D-BUS Service]
-                Name=${interface_name}
-                Exec=$out/bin/${pname}
-                User=root
-                SystemdService=${pname}.service
-                END
+                    mkdir -p $out/bin
+                    mv vgsc $out/bin/${pname}
 
-                # D-Bus config file
-                mkdir -p $out/share/dbus-1/system.d
-                cp ${./dbus.conf.xml} $out/share/dbus-1/system.d/${interface_name}.conf
-            '';
+                    runHook postInstall
+                '';
+
+                postInstall = ''
+                    # Launch D-Bus service
+                    mkdir -p $out/share/dbus-1/system-services
+                    cat <<END > $out/share/dbus-1/system-services/${interface_name}.service
+                    [D-BUS Service]
+                    Name=${interface_name}
+                    Exec=$out/bin/${pname}
+                    User=root
+                    SystemdService=${pname}.service
+                    END
+
+                    # D-Bus config file
+                    mkdir -p $out/share/dbus-1/system.d
+                    cp ${./dbus.conf.xml} $out/share/dbus-1/system.d/${interface_name}.conf
+                '';
+            };
         };
         nixosModules.default = {...}: let
-            package = self.packages.${pkgs.stdenv.hostPlatform.system}.default;
+            daemonPkg = self.packages.${pkgs.stdenv.hostPlatform.system}.daemon;
+            interfacePkg = self.packages.${pkgs.stdenv.hostPlatform.system}.interface;
         in {
-            services.dbus.packages = [package];
-            # environment.systemPackages = [package];
+            services.dbus.packages = [daemonPkg];
+            environment.systemPackages = [interfacePkg];
 
-            systemd.services.${binary_name} = {
+            systemd.services.${binary_daemon_name} = {
                 description = "Vinii's Graphical System Controller Daemon";
                 wantedBy = ["multi-user.target"];
                 after = ["dbus.service"];
@@ -86,11 +107,11 @@
                     XDG_RUNTIME_DIR = "/run/user/1000";
                     DBUS_SESSION_BUS_ADDRESS = "unix:path=/run/user/1000/bus";
                 };
-                path = runtimePackages;
+                path = daemonRuntimePackages;
                 serviceConfig = {
                     Type = "dbus";
                     BusName = interface_name;
-                    ExecStart = "${package}/bin/${binary_name}";
+                    ExecStart = "${daemonPkg}/bin/${binary_daemon_name}";
                     Restart = "always";
                 };
             };
